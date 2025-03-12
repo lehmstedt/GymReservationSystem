d6548f097bf35985b95a492efa29d75ffc1e737c
Sebastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2025-03-11 21:40:54 -0400

    Implemented Blazor Client for displaying classes

---
+++ Configurator/ReservationSystemConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Infrastructure;
+
+namespace Configurator;
+
+public static class ReservationSystemConfigurator
+{
+    public static ReservationSystem ConfigureReservationSystem()
+    {
+        var wod = new GymClass(new ClassId(), "WOD");
+        var spinning = new GymClass(new ClassId(), "Spinning");
+        var port = new InMemoryReservationPort([wod, spinning]);
+
+        return new ReservationSystem(port);
+    } 
+}
